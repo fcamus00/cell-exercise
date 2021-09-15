a2c1b73975c636ecb3cf71d2dea04bf5a665d699
Felipe Camus <felipe.camus.d@gmail.com>
2021-09-15 01:13:35 -0300

    app working

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,128 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const defaultGrid = [
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,2,1,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,2,1,0,0,0,0,0],
+    [0,0,0,0,0,0,0,2,1,0,0,0,0,0,0,0,0,2,0,0,0,0,0,1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,1,2,0,0,0,0,0,0,0,0,2,0,0,0,1,0,1,2,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+  ];
+  const [grid, setGrid] = React.useState(defaultGrid);
+  const [animate, setAnimate] = React.useState(false);
+  const colors: { [key: number]: string } = {
+    0: '#a3a3a3',
+    1: '#5daaff',
+    2: '#2cb48a'
+  };
+
+  React.useEffect(() => {
+    async function getData() {
+      const seed = await axios.get('coding-project.imtlab.io/seed');
+      if (seed.data) {
+        setGrid(seed.data.data.state);
+      }
+    }
+    getData();
+  }, []);
+
+  const getMatrixValue = (matrix: number[][], y: number, x: number) => {
+    let value;
+    try {
+      value = matrix[y][x] !== undefined ? matrix[y][x] : null;
+    } catch (error) {
+      value = null;
+    }
+    return value;
+  };
+
+  const findNeighbors = (matrix: number[][], y: number, x: number) => {
+    const neighbors = [];
+    const detail = {
+      dead: 0,
+      sad: 0,
+      happy: 0,
+    };
+    neighbors.push(getMatrixValue(matrix, y-1, x-1));
+    neighbors.push(getMatrixValue(matrix, y-1, x));
+    neighbors.push(getMatrixValue(matrix, y-1, x+1));
+    neighbors.push(getMatrixValue(matrix, y, x+1));
+    neighbors.push(getMatrixValue(matrix, y+1, x+1));
+    neighbors.push(getMatrixValue(matrix, y+1, x));
+    neighbors.push(getMatrixValue(matrix, y+1, x-1));
+    neighbors.push(getMatrixValue(matrix, y, x-1));
+    neighbors.forEach((item) => {
+      if (item === 0) {
+        detail.dead += 1;
+      } else if (item === 1) {
+        detail.sad += 1;
+      } else if (item === 2) {
+        detail.happy += 1;
+      }
+    });
+    return detail;
+  };
+
+  const newMatrix = (matrix: number[][]) => {
+    const newMatrix = JSON.parse(JSON.stringify(matrix));
+    for (let y = 0; y < matrix.length; y++) {
+      for (let x = 0; x < matrix[y].length; x++) {
+        const neighbors = findNeighbors(matrix, y, x);
+        const happySad = neighbors.happy + neighbors.sad;
+        if (matrix[y][x] === 0) {
+          if (neighbors.sad >= 3 && neighbors.sad > neighbors.happy) {
+            newMatrix[y][x] = 1;
+          } else if (neighbors.happy >= 3 && neighbors.happy > neighbors.sad) {
+            newMatrix[y][x] = 2;
+          }
+        } else {
+          if (happySad !== 2 && happySad !== 3) {
+            newMatrix[y][x] = 0;
+          }
+        }
+      }
+    }
+    return newMatrix;
+  };
+
+  React.useEffect(() => {
+    while (animate) {
+      const interval = setInterval(() => {
+        setGrid(newMatrix(grid));
+      }, 1000);
+      return () => clearInterval(interval);
+    }
+  });
+
+  const handleAnimation = () => {
+    setAnimate(!animate);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {grid ? grid.map((row: number[]) => (
+        <div className="row">
+          {row.map((cell) => (
+            <div className="cell" style={{backgroundColor: colors[cell]}}></div>
+          ))}
+        </div>
+      )): null}
+      <div>
+        <button onClick={() => handleAnimation()}>{animate ? 'Pause' : 'Start'}</button>
+      </div>
     </div>
   );
 }
